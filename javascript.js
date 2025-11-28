7371399a5b97642ec271ce7cc2d945105f9693d9
paqua26 <salcidopay@gmail.com>
2025-11-27 21:47:58 -0600

    Create computer decision Create the function that returns randomly a choice between rock/paper/scissor as literally string.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log('Hello, World!');
+function getComputerChoice(){
+
+    //random number between 0-2
+    switch (Math.floor(Math.random() * 3)){
+        case 0: return 'rock';
+        case 1: return 'paper';
+        case 2: return 'scissors';
+    }
+}
+
+
+print(getComputerChoice());
+
+
+
+function print(message){
+    console.log(message);
+}
